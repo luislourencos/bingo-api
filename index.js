9f51b2b79df35d2ae3f234b95b997e9436f83606
Sergio Miguel Lourenco <sergio.miguel.sge@mango.com>
2024-01-07 23:12:17 +0100

    feature/give more funcioinality

--- index.js
+++ index.js
@@ -1,19 +1,71 @@
 
 require('dotenv').config();
-const socketIo = require('socket.io')
 const app = require('express')();
-const {createCard} = require('./bingo');
-const { env: { PORT, API_URL } } = process
-
+const http = require('http').Server(app);
 const cors = require('cors');
+const {createCard} = require('./bingo');
+const { env: { PORT } } = process
 
-app.use(cors(
-    {
+app.use(cors());
+
+const socketIO = require('socket.io')(http, {
+    cors: {
         origin: "*",
         credentials: true
     }
-));
+});
 
+var userList = [];
+var winnerFirstLine = { line: false, user: '' };
+var winnerBingo = { bingo: false, user: '' };
+socketIO.on('connection', (socket) => {
+
+    socket.on('restart', () => {
+        userList = [];
+        winnerFirstLine = { line: false, user: '' };
+        winnerBingo = { bingo: false, user: '' };
+        socket.broadcast.emit('winnerFirstLine', { line: false, user: '' });
+        socket.broadcast.emit('winnerBingo', { bingo: false, user: '' });
+        socket.broadcast.emit('randomNumbers',{numbers:[]});
+        socket.broadcast.emit('userList', []);
+    });
+
+    socket.on('winnerFirstLine', (data) => {
+        winnerFirstLine = data;
+        socket.broadcast.emit('winnerFirstLine', data);
+    })
+
+    socket.on('winnerBingo', (data) => {
+        winnerBingo = data;
+        console.log('winnerBingo', data);
+        socket.broadcast.emit('winnerBingo', data);
+    })
+
+    socket.on('user', (data) => {
+        const {name} = data;
+        const index = userList.findIndex((item) => item.name === name);
+
+        const percCard0 = data.card[0]?.filter((item) => item.matched).length;
+        const percCard1 = data.card[1]?.filter((item) => item.matched).length;
+        const percCard2 = data.card[2]?.filter((item) => item.matched).length;
+        const completed = Math.round((percCard0 + percCard1 + percCard2) / 15 * 100);
+      
+        if (index !== -1) {
+            userList[index] = {...data, completed};
+        } else {
+            userList.push({ ...data, completed});
+        }
+     
+        socket.broadcast.emit('userList', userList);
+    });
+
+    socket.on('randomNumbers', (data) => {
+        socket.broadcast.emit('randomNumbers', data);
+    });
+    socket.on('disconnect', () => {
+      console.log('🔥: A user disconnected');
+    });
+});
 
 
 app.get('/api/getCard', (_req, res) => {
@@ -21,6 +73,6 @@ app.get('/api/getCard', (_req, res) => {
     res.json(card).status(200);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-} );
+http.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
